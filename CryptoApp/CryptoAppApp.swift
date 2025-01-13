d81625d4b91e9c97185d7a1174318c14170941d7
DJ Cartwright <dj.cartwright.web@gmail.com>
2025-01-13 10:11:28 -0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by David Cartwright on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
