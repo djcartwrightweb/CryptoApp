48e6624da6d4275efc067a51084aecf931865ada
DJ Cartwright <dj.cartwright.web@gmail.com>
2025-01-16 11:37:19 -0500

    complete lesson #9

--- CryptoApp/Services/CoinImageService.swift
+++ CryptoApp/Services/CoinImageService.swift
@@ -16,14 +16,17 @@ class CoinImageService {
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
     private let fileManager = LocalFileManager.shared
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
-        
+        self.imageName = coin.id
+        getCoinImage()
     }
     
     private func getCoinImage() {
-        if let savedImage = fileManager.getImage(imageName: coin.id, folderName: "coin_images") {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: "coin_images") {
             image = savedImage
             print("retreived image from File Manager")
         } else {
@@ -42,9 +45,13 @@ class CoinImageService {
             .sink(
                 receiveCompletion: NetworkingManager.handleCompletion,
                 receiveValue: { [weak self] (returnedImage) in
-                    self?.image = returnedImage
-                    self?.imageSubscription?.cancel()
-                    self?.fileManager.saveImage(image: <#T##UIImage#>, imageName: <#T##String#>, folderName: <#T##String#>)
+                    guard
+                        let self = self,
+                        let downloadedImage = returnedImage
+                    else {return}
+                    self.image = returnedImage
+                    self.imageSubscription?.cancel()
+                    self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
